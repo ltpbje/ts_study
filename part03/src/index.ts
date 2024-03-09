fa5c1290dc34fa56694289a0015eb7a04e9d1fec
乐天居易 <19836788252@163.com>
2024-03-09 17:09:12 +0800

    使用webpack打包ts代码(2)

--- part03/src/index.ts
+++ part03/src/index.ts
@@ -1,6 +1,8 @@
 console.log('haha')
+import { hi } from "./m1"
 function sum(a :number, b :number) :number
 {
     return a + b
 }
 console.log(sum(2,3))
+console.log('m1中引入的',hi);
--- part03/webpack.config.js
+++ part03/webpack.config.js
@@ -1,6 +1,11 @@
 const path = require("path")
+// 引入 自动生成html文件 的插件
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const { Extension } = require("typescript")
 // webpack中的所有的配置信息都应该写在module.exports中
 module.exports = {
+    mode: 'development',  
     // 指定入口文件
     entry:'./src/index.ts',
     // 指定打包文件所在目录
@@ -21,6 +26,18 @@ module.exports = {
                 exclude:/node_modules/
             }
         ]
+    },
+    // 配置webpack插件
+    plugins:[
+        new CleanWebpackPlugin(),
+        new HTMLWebpackPlugin({
+            template:'./src/index.html'
+        })
+    ],
+    // 用来设置引用模块
+    resolve:{
+        extensions:['.ts','.js']
     }
+    
 
 } 
