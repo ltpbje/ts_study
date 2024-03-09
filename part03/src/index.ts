2aff6c6bf4571ff453824a7a38f0d698a6d9ae0f
乐天居易 <19836788252@163.com>
2024-03-09 21:22:07 +0800

    使用webpack打包ts代码(3)完成一部分

--- part03/src/index.ts
+++ part03/src/index.ts
@@ -4,5 +4,8 @@ function sum(a :number, b :number) :number
 {
     return a + b
 }
+const obj = {name: '孙悟空' , age:18}
+console.log(obj)
+console.log(obj.name)
 console.log(sum(2,3))
 console.log('m1中引入的',hi);
--- part03/webpack.config.js
+++ part03/webpack.config.js
@@ -5,7 +5,7 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const { Extension } = require("typescript")
 // webpack中的所有的配置信息都应该写在module.exports中
 module.exports = {
-    mode: 'development',  
+    mode: 'production',  
     // 指定入口文件
     entry:'./src/index.ts',
     // 指定打包文件所在目录
@@ -21,7 +21,33 @@ module.exports = {
                 // test指定的是规则生效的文件
                 test:/\.ts$/,
                 // 要使用的loader
-                use:'ts-loader',
+                use:[
+                    // 配置babel
+                    {
+                        // 指定加载器
+                        loader:"babel-loader",
+                        // 设置babel
+                        options:{
+                            presets:[
+                                [// 指定环境的插件
+                                "@babel/preset-env",
+                                // 配置信息
+                                {
+                                    // 要兼容的目标浏览器
+                                    "targets":{
+                                        "chrome": "100"
+                                    },
+                                    // 指定corejs的版本
+                                    "corejs":"3",
+                                    // 使用corejs的方式 "usage"表示按需加载
+                                    "useBuiltIns":"usage"
+                                }]
+                            ]
+                        },
+
+                    },
+                    'ts-loader'
+                ],
                 // 要排除的文件
                 exclude:/node_modules/
             }
