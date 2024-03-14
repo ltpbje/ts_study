f88b31439d7ef3dca50df692879654a0891bfebd
乐天居易 <19836788252@163.com>
2024-03-14 18:46:00 +0800

    泛型

--- chapter02/part01/src/08_泛型.ts
+++ chapter02/part01/src/08_泛型.ts
@@ -13,3 +13,18 @@ function fn<T>(a:T):T{
 // 可以直接调用具有泛型的函数
 let result = fn(10) //泛型，TS可以自动对类型进行推断
 let result2 =  fn<string>('hello')//指定泛型
+// T extends Inter 表示泛型T必须时Inter实现类（子类）
+interface Inter {
+    length:number
+}
+// T extends Inter 表示泛型T必须时Inter实现类(子类)
+function fn3<T extends Inter>(a:T):number{
+    return a.length
+}
+class MyClass<T>{
+    name:T
+    constructor(name:T){
+        this.name =name
+    }
+}
+const mc = new MyClass<string>('小白')
