e0aa72264968b81f38fef59169e5769fbe587b4f
乐天居易 <19836788252@163.com>
2024-03-16 17:07:19 +0800

    Food类完成

--- chapter02/part02/src/index.ts
+++ chapter02/part02/src/index.ts
@@ -10,5 +10,49 @@ class Food{
         // 但实际上它是null，那么程序会在运行时崩溃。
         this.element  = document.getElementById('food')!
     }
+
+    // 获取食物X轴方向的坐标
+    get X(){
+        return this.element.offsetLeft
+    }
+    // 获取食物Y轴方向的坐标
+    get Y(){
+        return this.element.offsetHeight
+    }
+    // 修改食物的位置
+    change(){
+        // 生成一个随机的位置
+        // 食物的位置最小是0 最大是290
+        // 蛇移动一次就是一格 一格的大小就是10 所以就要求食物的
+        this.element.style.left= Math.round(Math.random()*290)+'px'
+        this.element.style.top= Math.round(Math.random()*290)+'px'
+    }
 }
 const food =  new Food()
+food.change()
+// 定义表示计分牌的类
+class ScorePannel{
+    // score和level用来记录分数和等级
+    score : number = 0
+    level : number = 1
+    // 分数和等级所在的元素，在构造函数中进行初始化
+    scoreEle :HTMLElement 
+    levelEle: HTMLElement
+
+    constructor(){
+        this.scoreEle = document.querySelector('#score')!
+        this.levelEle = document.querySelector('#level')!
+    }
+    // 添加分数
+    addScore(){
+        this.scoreEle.innerHTML = this.score++ +''
+    }
+
+    // 提升等级
+    addLevel(){
+        if(this.level < 5)
+        {
+            this.levelEle.innerHTML = this.level++ + ''    
+        }
+}
+}
